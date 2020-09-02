ad4764293b406ccec60c0242a48a072976def05d
Danilo Martins <danilo.santos.martins2018@gmail.com>
2020-09-02 00:41:09 -0300

    Configurando token no cadastro

--- src/Controllers/UsuarioController.js
+++ src/Controllers/UsuarioController.js
@@ -1,4 +1,5 @@
 import Usuario from "../model/Usuario";
+import * as Yup from "yup";
 
 class UsuarioController {
   async index(req, res) {
@@ -9,9 +10,41 @@ class UsuarioController {
     });
   }
 
+  // POST /registrar
   async store(req, res) {
+    const schema = Yup.object().shape({
+      nome: Yup.string().required(),
+      email: Yup.string().email().required(),
+      senhaHash: Yup.string().required(),
+      confirmarSenha: Yup.string().when("senhaHash", (senhaHash, field) =>
+        senhaHash ? field.required().oneOf([Yup.ref("senhaHash")]) : field
+      ),
+    });
+
+    const camposValidos = await schema.isValid(req.body);
+
+    console.log(camposValidos);
+
+    if (!camposValidos) {
+      return res.status(400).json({
+        error: "Preencha os campos corretamente!",
+      });
+    }
+
+    const { email } = req.body;
+
+    const usuarioExistente = await Usuario.findOne({
+      email,
+    });
+
+    if (usuarioExistente) {
+      return res.status(400).json({
+        error: "Email ja cadastrado",
+      });
+    }
+
     const usuario = await Usuario.create(req.body);
-    console.log(req.body);
+
     return res.json(usuario);
   }
 }
--- src/model/Usuario.js
+++ src/model/Usuario.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
-import uniqueValidator from "mongoose-unique-validator";
 const Schema = mongoose.Schema;
+import uniqueValidator from "mongoose-unique-validator";
+import jwt from "jsonwebtoken";
+const bcrypt = require("bcryptjs");
+import authConfig from "../config/secret";
 
 const UsuarioSchema = new mongoose.Schema(
   {
@@ -25,7 +28,10 @@ const UsuarioSchema = new mongoose.Schema(
       type: Array,
       default: ["cliente"],
     },
-    hash: { type: String },
+    senhaHash: {
+      type: String,
+      required: [true, "não pode ficar vazia."],
+    },
     salt: { type: String },
     recovery: {
       type: {
@@ -37,6 +43,58 @@ const UsuarioSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+UsuarioSchema.plugin(uniqueValidator, { message: "já está sendo utilizado" });
+
+UsuarioSchema.pre("save", async function (next) {
+  if (!this.isModified("senhaHash")) {
+    return next();
+  }
+  this.senhaHash = await bcrypt.hash(this.senhaHash, 8);
+});
+
+UsuarioSchema.statics = {
+    generateToken ({ id }) {
+      return jwt.sign({ id }, authConfig.secret, {
+        expiresIn: authConfig.ttl
+      })
+    }
+}
+
+  return jwt.sign(
+    {
+      id: this._id,
+      email: this.email,
+      nome: this.nome,
+      exp: parseFloat(exp.getTime() / 1000, 10),
+    },
+    secret
+  );
+};
+
+UsuarioSchema.methods.enviarAuthJSON = function () {
+  return {
+    _id: this._id,
+    nome: this.nome,
+    email: this.email,
+    loja: this.loja,
+    role: this.permissao,
+    token: this.gerarToken(),
+  };
+};
+
+// RECUPERACAO
+// UsuarioSchema.methods.criarTokenRecuperacaoSenha = function () {
+//   this.recovery = {};
+//   this.recovery.token = crypto.randomBytes(16).toString("hex");
+//   this.recovery.date = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
+//   return this.recovery;
+// };
+
+// UsuarioSchema.methods.finalizarTokenRecuperacaoSenha = function () {
+//   this.recovery = { token: null, date: null };
+//   return this.recovery;
+// };
 
 //Product é o Model
 export default mongoose.model("Usuario", UsuarioSchema);
